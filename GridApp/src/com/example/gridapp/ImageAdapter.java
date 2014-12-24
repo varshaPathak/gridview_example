0cca0002940743d800658914eb5e4b8b06f20686
varshaPathak <pathak.vmpathak.varsha@gmail.com>
2014-12-24 04:37:50 -0500

    v2 Grid View Revised
    1) Image adapter constructer added
    2) Context initialised in constructer

--- GridApp/src/com/example/gridapp/ImageAdapter.java
+++ GridApp/src/com/example/gridapp/ImageAdapter.java
@@ -1,10 +1,15 @@
 package com.example.gridapp;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class ImageAdapter extends BaseAdapter {
+    private  Context mContext;
+	public ImageAdapter(Context context) {
+		mContext=context;
+	}
 
 	@Override
 	public int getCount() {
