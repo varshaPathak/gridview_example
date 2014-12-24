dcf780a307253523ddf171aa8fa24fcccab9f8c3
varshaPathak <pathak.vmpathak.varsha@gmail.com>
2014-12-24 05:05:58 -0500

    Images added in image view image adapter is used It works

--- GridApp/src/com/example/gridapp/ImageAdapter.java
+++ GridApp/src/com/example/gridapp/ImageAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
     private  Context mContext;
@@ -14,7 +16,7 @@ public class ImageAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return mThumbIds.length;
 	}
 
 	@Override
@@ -30,19 +32,35 @@ public class ImageAdapter extends BaseAdapter {
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(int position, View converView, ViewGroup parent) {
+		
+		ImageView imageView;
+		if(converView==null) {
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(150,150));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(5, 5, 5, 5);
+			
+		} else {
+			imageView = (ImageView) converView;
+		}
+		imageView.setImageResource(mThumbIds[position]);
+		return imageView;
 	}
 	public Integer[] mThumbIds = {
-		R.drawable.sample_0, R.drawable.sample_1, R.drawable.sample_2,
-		R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_5,
-		R.drawable.sample_6, R.drawable.sample_7, R.drawable.sample_0, R.drawable.sample_1, R.drawable.sample_2,
-		R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_5,
-		R.drawable.sample_6, R.drawable.sample_7, R.drawable.sample_0, R.drawable.sample_1, R.drawable.sample_2,
-		R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_5,
-		R.drawable.sample_6, R.drawable.sample_7
-	};
+		R.drawable.sample_0, R.drawable.sample_1, 
+		R.drawable.sample_2, R.drawable.sample_3, 
+		R.drawable.sample_4, R.drawable.sample_5, 
+		R.drawable.sample_5, R.drawable.sample_6,
+		R.drawable.sample_7, R.drawable.sample_0,
+		R.drawable.sample_1, R.drawable.sample_2,
+		R.drawable.sample_3, R.drawable.sample_4, 
+		R.drawable.sample_5, R.drawable.sample_5,
+		R.drawable.sample_6, R.drawable.sample_7, 
+		R.drawable.sample_0, R.drawable.sample_1, 
+		R.drawable.sample_2, R.drawable.sample_3, 
+		R.drawable.sample_4, R.drawable.sample_5,
+		R.drawable.sample_5, R.drawable.sample_6,
+		};
 	
-
 }
